fd59185955db3aefe0ad1519d4b29f5bd61736b7
thetti27 <liveup2it>
2021-10-04 00:38:37 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/knowledgebase.xml

--- app/src/main/java/com/example/madproject2/fdCalculator.java
+++ app/src/main/java/com/example/madproject2/fdCalculator.java
@@ -1,4 +1,18 @@
 package com.example.madproject2;
 
-public class fdCalculator {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+    public class fdCalculator extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.fdcalculator);
+        }
+
+
 }
